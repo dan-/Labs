646ab3bd953edb9e52e3f7f75443bf538e2b8c2f
Dan <njskater1490@gmail.com>
2016-03-31 19:19:38 -0400

    Added files via upload

---
+++ Lab7_1.java
@@ -0,0 +1,55 @@
+// Daniel Fontana
+// CSC 161-02
+// Lab7_1
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Lab7_1 {
+
+public static void main(String[] args) throws IOException{
+		int i;
+		int x = 0;
+		int y = 0;
+		int total = 0;
+
+		// Open inputFile.txt and store int in userNum.
+		File infile = new File("inputFile.txt");
+		Scanner inputFile = new Scanner(infile);
+
+		i = inputFile.nextInt();
+
+		// Open outputFile.txt
+		FileWriter outFile = new FileWriter("outputFile.txt");
+		PrintWriter outputFile = new PrintWriter(outFile);
+
+		outputFile.println("The number entered is " + i + ".");
+
+		// Check if number is negative.
+		if(i < 0){
+			i *= -1;
+		}
+
+		// Reverse the number.
+		while(i != 0){
+			x = i % 10;
+			i = i / 10;
+			y = y * 10 + x;
+		}
+
+		// Write the numbers and add the total.
+		while(y != 0){
+			i = y % 10;
+			y = y / 10;
+			outputFile.print(i + " ");
+
+			total += i;
+		}
+
+		// Write the total.
+		outputFile.println("the numbers added equal " + total + ".");
+
+		inputFile.close();
+		outputFile.close();
+	}
+}
---
+++ Lab7_4.java
@@ -0,0 +1,42 @@
+// Daniel Fontana
+// CSC 161-02
+// Lab7_4
+
+import javax.swing.*;
+import java.lang.*;
+
+/*
+	This program determines if the entered number is a prime number.
+*/
+
+public class Lab7_4{
+	public static void main(String[] args){
+		String userNumber;
+		int userNum = 0;
+		int x = 1;
+		boolean prime = false;
+
+		while(userNum != -999){
+			userNumber = JOptionPane.showInputDialog("Enter a positive integer (-999 to terminate):");
+
+			if(userNum == -999){
+				System.out.println("Terminating the loop. Goodbye");
+			} else{
+				userNum = Integer.parseInt(userNumber);
+
+				if(userNum < 1){
+					System.out.println("Invalid entry. Oops.");
+				} else if(userNum == 2){
+					System.out.println("PRime!");
+				} else {
+					while(x <= userNum / 2){
+						if(userNum % x == 0){
+							JOptionPane.showMessageDialog(null, userNum, "TITLE", 1);
+						}
+						x++;
+					}
+				}
+			}
+		}
+	}
+}
